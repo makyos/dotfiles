50f8f65254b1f5741dab03fbaba0f48de0cdeabd
makyos <makyos@gmail.com>
2017-02-01 21:46:46 +0900

    tmux plugin

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOTS=(.bashrc .gitconfig .gitignore .git-prompt.sh .emacs.d .tmux.conf .git-completion.bash)
+DOTS=(.bashrc .gitconfig .gitignore .git-prompt.sh .emacs.d .tmux.conf .git-completion.bash .tmux)
 for file in ${DOTS[@]}; do
     [ -e $HOME/$file ] && rm -rf $HOME/$file
     ln -s $HOME/src/dotfiles/$file $HOME/$file \
